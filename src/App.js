4cc7ef1945d18bdcaa4eac27165d914ffdd46f42
syasu916 <scott.yasuhara@gmail.com>
2021-08-18 15:23:50 -0700

    Added expense Item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit MEEEEE<code>src/App.js</code> and save to reload.
         </p>
+        <ExpenseItem></ExpenseItem>
         <a
           className="App-link"
           href="https://reactjs.org"
